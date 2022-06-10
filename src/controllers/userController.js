0d94fa4ecaa76923ce863a05744831a8fa4fbf13
mltlima <miguellt@dcc.ufrj.br>
2022-06-10 00:31:00 -0300

    ranking

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,13 +5,25 @@ export async function getUser(req, res) {
     const { user } = JSON.parse(JSON.stringify(res.locals));
 
     try {
-        //const user = await connection.query(`SELECT * FROM "users" WHERE "id" = $1`, [id]);
-
         const urls = await connection.query(`SELECT * FROM "urls" WHERE "userId" = $1`, [id]);
         const urlViews = await connection.query(`SELECT SUM("viewCount") AS "totalViews" FROM "urls" WHERE "userId" = $1`, [id]);
         
         const data = {id, name: user.name, visitCount: urlViews.rows[0].totalViews, shortnedUrls: urls.rows};
         res.status(200).send(data);
+    } catch (error) {
+        res.sendStatus(404);
+    }
+}
+
+export async function ranking(req, res) {
+    try {
+        const users = await connection.query(`SELECT usr."id", usr."name", COUNT(url."id") AS "linksCount",
+         SUM(url."viewCount") AS "visitCount" 
+         FROM "users" usr 
+         LEFT JOIN "urls" url ON usr."id" = url."userId"
+         GROUP BY usr."id", usr."name"
+         ORDER BY "visitCount" DESC LIMIT 10`);
+        res.status(200).send(users.rows);
     } catch (error) {
         res.sendStatus(404);
     }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { getUser } from "../controllers/userController.js";
+import { getUser, ranking } from "../controllers/userController.js";
 import { validateToken } from "../middlewares/authMiddleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/users/:id", validateToken, getUser);
+userRouter.get("/ranking", ranking);
 
 export default userRouter;
