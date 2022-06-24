974e39e35ec81709abfbbff603979b0b9ede75a8
pedro imbrech <elprogramadorpedro@gmail.com>
2022-06-24 11:36:19 -0300

    rev

--- README.md
+++
@@ -1,5 +0,0 @@
-# projeto16-shortly
-
-### Try it on : https://miguel-projeto16-shortly.herokuapp.com/
-
-Simple Nodejs url shortner api
--- src/middlewares/validateMiddleware.js
+++
@@ -1,9 +0,0 @@
-export default function validateMiddleware(schema) {
-    return (req, res, next) => {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            return res.status(422).send(error.details[0].message);
-        }
-        next();
-    };
-}
